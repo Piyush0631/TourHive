7e1bbace6a551d943a981277837e5338eb4ee547
Piyush0631 <piyushray067@gmail.com>
2026-02-09 09:47:34 +0530

    Refactored some production Errror handling

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+
+  process.exit(1);
+});
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.database.replace(
   '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
+  process.env.DATABASE_PASSWORD,
 );
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -18,6 +25,23 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
