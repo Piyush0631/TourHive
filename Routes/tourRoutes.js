67e061f6ce62e6c5a5ba58ca77feb4651ba8dfe3
Piyush0631 <piyushray067@gmail.com>
2026-02-11 15:09:21 +0530

    Implemented basic JWT authentication and route protection

--- Routes/tourRoutes.js
+++ Routes/tourRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
 const router = express.Router();
-
 // router.param('id', tourController.checkID);
 router
   .route('/top-5-cheap')
@@ -10,7 +10,7 @@ router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 router
   .route('/:id')
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
-exports.getALLUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'route not yest defined',
+const User = require('./../models/userModel');
+const APIFeatures = require('./../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getALLUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
