15842cea79b15027505b1e3bea97ee6c2a99f5e6
Piyush0631 <piyushray067@gmail.com>
2026-02-22 21:07:34 +0530

    Added Indexes and geospatial queries

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,15 +2,18 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const Tour = require('./../../models/tourModel');
+const Review = require('./../../models/reviewModel');
+const User = require('./../../models/userModel');
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.database.replace(
+const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
 );
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
   })
@@ -18,10 +21,16 @@ mongoose
     console.log('DB connection successfull');
   });
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'),
+);
 // importing data into database
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('data successfully loaded!');
   } catch (err) {
     console.log(err);
@@ -32,6 +41,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('data successfully deleted!');
   } catch (err) {
     console.log(err);
